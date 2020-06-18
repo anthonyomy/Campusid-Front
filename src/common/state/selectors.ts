71be64c029ddfde3d9e5f9076675c3a89e1f8727
Jan.Vermeulen <jan.vermeulen@ricardo.ch>
2020-06-18 18:16:59 +0200

    update

--- src/common/state/selectors.ts
+++ src/common/state/selectors.ts
@@ -5,3 +5,4 @@ export const getProfileImage = (state: Record<string, any>) =>
 export const getErrorPage = (state: Record<string, any>) => state?.errorPage;
 export const getIsAdmin = (state: Record<string, any>) => state?.isAdmin;
 export const getIdboard = (state: Record<string, any>) => state?.user?.idboard;
+export const getClassId = (state: Record<string, any>) => state?.user?.classId;
